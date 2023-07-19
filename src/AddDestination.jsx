18021b1385e7b943a349d865cc8c9f13aca2b2a5
suthyscott <sutherlandscott21@gmail.com>
2023-07-19 14:56:29 -0600

    building out add form component

--- src/AddDestination.jsx
+++ src/AddDestination.jsx
@@ -1,10 +1,20 @@
-
+import { useState } from "react"
 
 const AddDestination = () => {
+    const [name, setName] = useState('')
+    const [notes, setNotes] = useState('')
+    const [imageURL, setImageURL] = useState('')
+    const [international, setInternational] = useState(false)
+
     return (
-        <div>
-            AddDestination 
-        </div>
+        <>
+            <form>
+                <input placeholder="name" onChange={e => setName(e.target.value)} />
+                <input placeholder="notes" onChange={e => setNotes(e.target.value)} />
+                <input type="checkbox" checked={international} onChange={() => setInternational(!international)}/>
+                <input placeholder="Image URL" onChange={e => setImageURL(e.target.value)} />
+            </form>
+        </>
     )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Header from './Header'
-import AddDestination from './AddDestination'
-import DestinationCard from './DestinationCard'
-import axios from 'axios'
+import { useState, useEffect } from "react"
+import "./App.css"
+import Header from "./Header"
+import AddDestination from "./AddDestination"
+import DestinationCard from "./DestinationCard"
+import axios from "axios"
 
 function App() {
-  const [destinations, setDestinations] = useState([])
+    const [destinations, setDestinations] = useState([])
 
-  useEffect(() => {
-    axios.get(`http://localhost:4545/api/destinations?apiKey=${import.meta.env.VITE_APP_API_KEY}`)
-      .then(res => setDestinations(res.data))
-  }, [])
+    useEffect(() => {
+        axios
+            .get(
+                `http://localhost:4545/api/destinations?apiKey=${
+                    import.meta.env.VITE_APP_API_KEY
+                }`
+            )
+            .then(res => setDestinations(res.data))
+    }, [])
 
-  console.log('hit App.jsx', destinations)
-  
-  return (
-    <>
-      <Header/>
-      <main>
-        {destinations.map(dest => {
-          return <DestinationCard name={dest.name} imageURL={dest.imageURL} notes={dest.notes} international={dest.international}/>
-        })}
-      </main>
-    </>
-  )
+    console.log("hit App.jsx", destinations)
+
+    return (
+        <>
+            <Header />
+            <div id="main-container">
+                <main>
+                    {destinations.map(dest => {
+                        return (
+                            <DestinationCard
+                                name={dest.name}
+                                imageURL={dest.imageURL}
+                                notes={dest.notes}
+                                international={dest.international}
+                            />
+                        )
+                    })}
+                </main>
+                <section>
+                    <AddDestination />
+                </section>
+            </div>
+        </>
+    )
 }
 
 export default App
