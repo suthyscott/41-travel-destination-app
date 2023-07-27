448f5bf310a8fb94c0cd4ddce382239f7f219312
suthyscott <sutherlandscott21@gmail.com>
2023-07-27 08:56:01 -0600

    added Formik

--- src/Pages/AddDestination.jsx
+++ src/Pages/AddDestination.jsx
@@ -1,36 +1,76 @@
 import { useState } from "react"
 import axios from "axios"
+import { Formik } from "formik"
 
-const AddDestination = ({getAllDestinations}) => {
-    const [name, setName] = useState('Cool place')
-    const [notes, setNotes] = useState('')
-    const [imageURL, setImageURL] = useState('')
-    const [international, setInternational] = useState(false)
+const AddDestination = ({ getAllDestinations }) => {
+    // const [name, setName] = useState('Cool place')
+    // const [notes, setNotes] = useState('')
+    // const [imageURL, setImageURL] = useState('')
+    // const [international, setInternational] = useState(false)
 
-    const handleSubmit = e => {
-        e.preventDefault()
+    const initialValues = {
+        name: "",
+        notes: "",
+        imageURL: "",
+        international: false
+    }
 
-        axios.post(`/api/destinations?apiKey=${import.meta.env.VITE_APP_API_KEY}`, {name, notes, imageURL, international})
+    const handleFormSubmit = values => {
+        console.log(values)
+
+        axios
+            .post(
+                `/api/destinations?apiKey=${import.meta.env.VITE_APP_API_KEY}`,
+                values
+            )
             .then(res => {
-                getAllDestinations()
-                setName('')
-                setNotes('')
-                setImageURL('')
-                setInternational(false)
+                // getAllDestinations()
+                // setName("")
+                // setNotes("")
+                // setImageURL("")
+                // setInternational(false)
             })
             .catch(err => console.log(err))
     }
 
     return (
-        <>
-            <form onSubmit={e => handleSubmit(e)} className="border-[3px] border-red-500 border-solid">
-                <input placeholder="name" value={name} onChange={e => setName(e.target.value)} />
-                <input placeholder="notes" value={notes} onChange={e => setNotes(e.target.value)} />
-                <input type="checkbox" checked={international} onChange={() => setInternational(!international)}/>
-                <input placeholder="Image URL" value={imageURL} onChange={e => setImageURL(e.target.value)} />
-                <button>Submit</button>
-            </form>
-        </>
+        <Formik initialValues={initialValues} onSubmit={handleFormSubmit}>
+            {({ values, handleChange, handleSubmit }) => {
+                return (
+                    <form
+                        onSubmit={handleSubmit}
+                        className="border-[3px] border-red-500 border-solid"
+                    >
+                        <input
+                            placeholder="name"
+                            value={values.name}
+                            onChange={handleChange}
+                            name='name'
+                        />
+                        <input
+                            placeholder="notes"
+                            value={values.notes}
+                            onChange={handleChange}
+                            name='notes'
+                        />
+                        <input
+                            type="checkbox"
+                            checked={values.international}
+                            value={values.international}
+                            onChange={handleChange}
+                            name="international"
+                        />
+                        <input
+                            placeholder="Image URL"
+                            value={values.imageURL}
+                            onChange={handleChange}
+                            name="imageURL"
+                        />
+                        <button>Submit</button>
+                    </form>
+                )
+            }}
+        </Formik>
     )
 }
 
